28d54a1c03ba47efce85551361b335fb5e2b9619
basilahamed07 <basilahamed46@gmail.com>
2024-08-09 12:35:05 +0530

    django project

--- schedule/schedule/asgi.py
+++ schedule/schedule/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for django_pratice_1 project.
+ASGI config for schedule project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_pratice_1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'schedule.settings')
 
 application = get_asgi_application()
