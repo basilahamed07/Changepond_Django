9b02bc9032c167545bb0f66fac565c8f87b1576b
basilahamed07 <basilahamed46@gmail.com>
2024-08-09 15:49:31 +0530

    task completed

--- Day_5 Django/schedule/month/views.py
+++ Day_5 Django/schedule/month/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+from django.http import *
 
+# sample function
+Month = {
+    "jan":"Learning Python",
+    "feb":"sleeping",
+    "may":"do nothing",
+    "april":"april Fool"
+}
+
+def month_details(request,month):
+    try:
+        month_text = Month[month]
+        return HttpResponse(f"<h1>{month_text}<h1>")
+    except:
+        return HttpResponseNotFound(f"<h1>{month} Not Found <h1>")
 # Create your views here.
     
